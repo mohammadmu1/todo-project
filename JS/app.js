000ae4a07edfa4c5c1a5bce2761e51574d59b885
Mohammad mustafa <133863751+mohammadmu1@users.noreply.github.com>
2023-06-02 15:35:36 +0300

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,13 @@
-let name =prompt("please , Enter your name")
-let gender =prompt("please , Enter your gender\nThe answer should be either (male or female).")
-let age = prompt("please , Enter your age")
-while(age<=0){
-    alert("the age must be a number and greater than 0")
-    age = prompt("please , Enter your age")
-}
 confirmWelcoming=confirm("Do you  wants to skip the welcoming message.")
-
-
 if(!confirmWelcoming){
-
+    let name =prompt("please , Enter your name")
+    let gender =prompt("please , Enter your gender\nThe answer should be either (male or female).")
+    let age = prompt("please , Enter your age")
+    while(age<=0){
+        alert("the age must be a number and greater than 0")
+        age = prompt("please , Enter your age")
+    }
+    
 
     if(gender=='male' || gender=='MALE'){
         alert("Welcome Mr " + name)
@@ -24,3 +22,6 @@ if(!confirmWelcoming){
     }
 
 }
+
+
+
