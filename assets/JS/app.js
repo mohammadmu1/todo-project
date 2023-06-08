b9b72e5cb118dbc44dfba50f76e757182e44f962
mohammadmu1 <mbn.1991@yahoo.com>
2023-06-08 21:47:49 +0000

    js2

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,61 +1,91 @@
+//Functions Part
 function printArrayElemnts(arr){
     let l=arr.length
     for (let i =0; i<l ;i++){
-        console.log(arr[i]);
+        console.log(i +" : "+arr[i]);
     }
 }
 
+const yesNoQustion = (qustion) =>{
 
-let arr =[];
-confirmWelcoming=confirm("Do you  wants to skip the welcoming message.")
+    let ans = prompt(qustion);
 
-
-if(!confirmWelcoming){
-    let name =prompt("please , Enter your name" , " ")
-    if(name==null || name ==' '){
-        name = "invalid"
+    if (ans === 'Yes' || ans === 'yes'||ans ==='YES'){
+        return 'Yes';
     }
-    
-    arr.push(name)
-    
+    else if (ans === 'No' || ans ==='no'||ans ==='NO'){
+        return 'No';
+    }
+    else {
+        return "invaled";
+    }
+}
+
+const whatIsYourName = ()=>{
+    return prompt("please , Enter your name" , " ")
+}
+
+
+const gender=()=>{
     let gender =prompt("please , Enter your gender\nThe answer should be either (male or female).")
     while(true){
         if (gender =="male" || gender =="female"){
             break;
         }
-        alert("The answer should be either (male or female).")
-        gender =prompt("please , Enter your gender.")
+        alert("The answer should be either (male or female).");
+        gender =prompt("please , Enter your gender.");
         
     }
-    arr.push(gender)
+    return gender;
+}
 
-
+const whatIsYourAge = ()=>{
     let age = prompt("please , Enter your age")
     while(age<=0){
         alert("the age must be a number and greater than 0")
         age = prompt("please , Enter your age")
     }
-    arr.push(age)
+    return age;
+}
 
-    let isHavinJob=confirm("do you have a job.")
-    arr.push(isHavinJob)
-
-    let isLoveCoding=confirm("do you have love coding.")
-    arr.push(isLoveCoding)
-
-    let isFreeToday=confirm("Are you free today.")
-    arr.push(isFreeToday)
-    
+function welcomngMessage(gender , name){
     if(gender=='male' ){
         alert("Welcome Mr " + name)
     }
     else  {
         alert("Welcome Ms " + name)
     }
-    printArrayElemnts(arr)
+}
 
+//End of Functions Part
+
+
+//main
+let yesNoAnswers=[];
+
+
+let confirmWelcoming=confirm("Do you  wants to skip the welcoming message.")
+if(!confirmWelcoming){
+    let name = whatIsYourName();
+    let gender = gender();
+    whatIsYourAge();
+    welcomngMessage(gender , name );
+    
 }
 
 
+let confirmNoreQustions=confirm("Do you  wants to skip more 3 qustions")
+if(!confirmNoreQustions){
+    yesNoAnswers.push(
+        yesNoQustion("do you have a job.")
+    )
 
+    yesNoAnswers.push(
+        yesNoQustion("do you have love coding.")
+    )
 
+    yesNoAnswers.push(
+        yesNoQustion("Are you free today.")
+    )
+    printArrayElemnts(yesNoAnswers);
+}
